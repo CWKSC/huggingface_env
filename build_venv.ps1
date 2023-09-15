ec2edb44aa90b76536d1311f82c19c752608e9ff
CWKSC <53114952+CWKSC@users.noreply.github.com>
2023-09-16 00:07:03 +0800

    Update

---
+++ build_venv.ps1
@@ -0,0 +1,13 @@
+py -3.9 -m venv venv
+.\venv\Scripts\activate
+
+python -m pip install --upgrade pip
+
+pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121
+pip install huggingface_hub
+pip install git+https://github.com/huggingface/transformers.git
+
+# https://huggingface.co/docs/transformers/main_classes/quantization
+pip install auto-gptq
+pip install git+https://github.com/huggingface/optimum.git
+pip install --upgrade accelerate
